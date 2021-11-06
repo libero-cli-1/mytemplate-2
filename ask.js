b8be66fe5dd760ee25709dafecca94f3b642df88
zhaoshihao <1962710675@qq.com>
2021-11-06 12:38:59 +0800

    chore(build):editor ask.js && package.json

--- ask.js
+++ ask.js
@@ -1,25 +1,22 @@
-module.exports=[
-	{
-		type:'confirm',
-		name:'private',
-		mesage:'the registery is private ?'
-	},
-	{
-		type:'input',
-		name:'author',
-		message:'author',
-
-	},
-	{
-		type:'input',
-                name:'description',
-                message:'description?',
-	},
-	{
-		type:'input',
-                name:'license',
-                message:'license?',
-
-	}
-
-]
+module.exports = [
+  {
+    type: "confirm",
+    name: "private",
+    mesage: "the registery is private ?",
+  },
+  {
+    type: "input",
+    name: "author",
+    message: "author",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "description?",
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "license?",
+  },
+];
