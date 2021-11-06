2b003e7cca7d6207d578589f17034780295845dc
zhaoshihao <1962710675@qq.com>
2021-11-06 11:34:24 +0800

    chore(ask.js): editor ask.js

---
+++ ask.js
@@ -0,0 +1,25 @@
+module.exports=[
+	{
+		type:'confirm',
+		name:'private',
+		mesage:'the registery is private ?'
+	},
+	{
+		type:'input',
+		name:'author',
+		message:'author',
+
+	},
+	{
+		type:'input',
+                name:'description',
+                message:'description?',
+	},
+	{
+		type:'input',
+                name:'license',
+                message:'license?',
+
+	}
+
+]
